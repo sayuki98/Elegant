9b45e8af24acf24ee42507c49d032b4a886d4fc0
sayuki98 <sayuki1998@gmail.com>
2019-08-18 20:11:01 +0300

    forgot to commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+$("#navToggle").click(function() {
+    $(this).toggleClass("active");
+    $(".overlay").toggleClass("open");
+    // this line ▼ prevents content scroll-behind
+    $("body").toggleClass("locked");
+});
+
